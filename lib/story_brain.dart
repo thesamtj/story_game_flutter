c518a3f42a56851763cfa5a8d7b1765fd82d082c
Samuel Tijani <omalitijani@gmail.com>
2020-08-23 19:53:30 +0100

    Update the Story

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -58,7 +58,7 @@ class StoryBrain {
 //TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
   int storyNumber = 0;
 //TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
-
+  void nextStory(int choiceNumber) {}
 //TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
 //TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
