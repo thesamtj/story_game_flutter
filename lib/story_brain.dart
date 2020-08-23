1c9866a00e9b86b5f5eb2d02ef9f9446995d2687
Samuel Tijani <omalitijani@gmail.com>
2020-08-23 20:56:05 +0100

    Resets the storyNumber

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -80,7 +80,9 @@ class StoryBrain {
 //TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-  restart() {}
+  restart() {
+    storyNumber = 0;
+  }
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
 
 }
